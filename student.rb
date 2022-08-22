51a9f50d0e350f99684cdc1b8a7dc09928ddd2ef
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-08-22 18:22:51 -0300

    Add student class

---
+++ student.rb
@@ -0,0 +1,13 @@
+require './person'
+
+# Student class that extends from person
+class Student < Person
+  def initialize(age, classroom, name = 'Unknown', parent_permision: true)
+    super(age, name, parent_permision: parent_permision)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯\\(ツ)/¯'
+  end
+end
