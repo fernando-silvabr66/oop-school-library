be3a8ca326614d12047afce1db45ed881e9c2f2b
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-08-22 18:20:39 -0300

    Add person class

---
+++ person.rb
@@ -0,0 +1,44 @@
+# Parent class for teacher and student
+
+class Person
+
+  attr_reader :id
+
+  attr_accessor :age, :name
+
+
+
+  def initialize(age, name = 'Unknown', parent_permision: true)
+
+    @id = Random.rand(1..100)
+
+    @name = name
+
+    @age = age
+
+    _parent_permision = parent_permision
+
+  end
+
+
+
+  private
+
+
+
+  def of_age?
+
+    age >= 18
+
+  end
+
+
+
+  def can_use_services?
+
+    of_age? || parent_permision
+
+  end
+
+end
+
